1210177eea8e3075ecbc59d80aee3456ed1118ee
Wojciech Ossowski <zavafoj@gmail.com>
2019-01-06 17:41:26 +0100

    Added omdb query endpoint

---
+++ providers/OpenMoviesProvider.js
@@ -0,0 +1,11 @@
+const apiKey = process.env.EXT_OMDB_APIKEY;
+
+exports.fetchMovie = async function (title) {
+    return fetch(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
+        .then(res => res.json());
+}
+
+exports.searchMovie = async function (title) {
+    return fetch(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
+        .then(res => res.json());
+}
