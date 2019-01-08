a26181afae3365777918e8d81219515f3747b77d
Wojciech Ossowski <zavafoj@gmail.com>
2019-01-08 15:57:30 +0100

    Fixing mocha exitting and enabling test server configuration

--- migrations/20190106184250_setup.js
+++ migrations/20190106184250_setup.js
@@ -41,7 +41,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
     return Promise.all([
-        knex.schema.dropTable('movies'),
         knex.schema.dropTable('comments'),
+        knex.schema.dropTable('movies'),
     ]);
 };
--- models/movies.test.js
+++ models/movies.test.js
@@ -3,11 +3,12 @@ const path = require('path')
 const { assert, expect } = require('chai')
 const movies = require('./movies');
 
-describe('MoviesModel test', function() {
+describe('MoviesModel test', function(done) {
     
-    it('selects all movies', async function (done) {
-        var storedMovies = await movies.getAllMovies();
-        done()
+    it('selects all movies', function (done) {
+        movies.getAllMovies().then(function (movies) {
+            done();
+        });
     });
 
 });
