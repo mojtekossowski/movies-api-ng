3f95c829df636109c107bf147ef7b6e6471d982c
Wojciech Ossowski <zavafoj@gmail.com>
2019-01-10 15:31:54 +0100

    Adding engines info

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-10T14:02:30.509Z",
+    "time": "2019-01-10T14:28:57.010Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
